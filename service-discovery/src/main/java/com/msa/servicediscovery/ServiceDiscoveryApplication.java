f5cc3bc2c9a984710cc35da5ce37296351d9293d
leebeomjun <qjawnswkd0717@gmail.com>
2021-04-12 20:26:46 +0900

    eureka 서버 설정

--- service-discovery/src/main/java/com/msa/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/msa/servicediscovery/ServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.msa.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer	//eureka 서버로 설정 (service discovery 기능)
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
